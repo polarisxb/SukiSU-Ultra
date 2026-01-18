a121ba6cc99134c7040fc69a21dbe3466782b82a
Polaris <3230431674@qq.com>
2026-01-18 20:49:56 +0800

    fix(ksud): export ksu_handle_vfs_read for external linkage

--- kernel/ksud.c
+++ kernel/ksud.c
@@ -365,7 +365,7 @@ static ssize_t read_iter_proxy(struct kiocb *iocb, struct iov_iter *to)
     return ret;
 }
 
-static int ksu_handle_vfs_read(struct file **file_ptr, char __user **buf_ptr,
+int ksu_handle_vfs_read(struct file **file_ptr, char __user **buf_ptr,
                                size_t *count_ptr, loff_t **pos)
 {
 #ifndef KSU_KPROBES_HOOK
